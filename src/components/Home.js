3c6cbd79f6c2e1a5b249bd476b71ad88c86936d9
Gary Sentosa <gary.sentosa@gmail.com>
2020-12-20 21:42:49 +0900

    Add search by prefecture form template

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <p>ekidataへようこそ！</p>
+    <div className="row">
+      <p>ekidataへようこそ！</p>
+    </div>
   );
 }
 
--- src/components/SearchByPrefecture.js
+++ src/components/SearchByPrefecture.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react'
 
-const SearchByPrefecture = () => {
-  return  (
-    <p>都道府県で路線/駅を検索</p>
-  );
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+
+class SearchByPrefecture extends Component {
+  render() {
+    return  (
+      <>
+        <div className="row">
+          <p>都道府県で路線/駅を検索</p>
+        </div>
+        <div className="row">
+          <div className="col px-0">
+            <SearchForm/>
+          </div>
+        </div>
+      </>
+    );
+  }
 };
+
+function SearchForm() {
+  return (
+    <Form>
+      <Form.Row>
+        <Form.Group as={Col}>
+          <Form.Control as="select" defaultValue="都道府県を選択">
+            <option value="">都道府県を選択</option>
+         </Form.Control>
+        </Form.Group>
+        <Form.Group as={Col}>
+          <Form.Control as="select" defaultValue="路線を選択">
+            <option value="">路線を選択</option>
+         </Form.Control>
+        </Form.Group>
+        <Form.Group as={Col}>
+          <Form.Control as="select" defaultValue="駅を選択">
+            <option value="">駅を選択</option>
+         </Form.Control>
+        </Form.Group>
+      </Form.Row>
+      <Button variant="primary" type="submit">検索</Button>
+    </Form>
+  )
+}
 
 export default SearchByPrefecture;
